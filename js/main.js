07d1c9d3a7b17333d77f36f24539e4952f5c9c83
Leinda Demy <118118113+leindademy@users.noreply.github.com>
2022-11-14 01:08:10 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Click On toggle show nav
+
+let nav = document.querySelector('.links');
+let toggle = document.getElementById("toggle").onclick = function () {
+  nav.classList.toggle("show");
+}
+
+
